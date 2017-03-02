ad13b67134d2bda1fab5e2b44a2abe27a3b5504f
Hendro Steven Tampake <hendro.steven@gmail.com>
2017-03-02 10:33:54 +0800

    Update CrudApplicationTests.java
    sample test

--- src/test/java/com/hendro/CrudApplicationTests.java
+++ src/test/java/com/hendro/CrudApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CrudApplicationTests {
-
+	//sample test
 	@Test
 	public void contextLoads() {
 	}
